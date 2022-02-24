3d842aa344b314760131596e817ac6341d1e9490
Kseniya-Korolchuk <kkorolchuk@gmail.com>
2022-02-25 00:53:30 +0300

    feat: implement check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let openedBr = [];
+  let closedBr = [];
+  let openedStr = [];
+  
+  bracketsConfig.forEach( function(br){
+    openedBr.push(br[0]);
+    closedBr.push(br[1]);
+  });
+
+  function close(br) {
+    if (openedStr[openedStr.length - 1] == openedBr[closedBr.indexOf(br)]) {
+      openedStr.pop();
+    }
+  }
+
+  for (let ch of str) {
+    if (closedBr.includes(ch) && openedStr.includes(openedBr[closedBr.indexOf(ch)])) {
+      close(ch);
+    }
+    else if (openedBr.includes(ch)) {
+      openedStr.push(ch);
+    } 
+    else {
+      return false;
+    }
+  }
+
+  return (!openedStr.length) ? true : false;
 }
